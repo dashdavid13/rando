6ab953da29136b9f81f53b793c972bebc9acdf14
David <davidamedina619@gmail.com>
2021-08-03 12:14:50 -0400

    testing

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>test</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +20,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
